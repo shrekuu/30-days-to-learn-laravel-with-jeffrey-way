c0d262af1e95f378f90c368a71901a7471c8d02e
Bond <bond@aspecta.ai>
2024-04-11 21:38:39 +0800

    feat: Ep 02 - Your First Route and View

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>About Page</title>
+    </head>
+    <body>
+        <h1>Hello from the Contact Page.</h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    // return 'About Page';
+
+    // return ['foo' => 'bar'];
+
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
